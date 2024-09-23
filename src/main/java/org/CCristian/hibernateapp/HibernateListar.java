6e0e50d62d5a26aefe22d71bfd368442d1dbf931
CCrisstian <crisstiann.c@gmail.com>
2024-09-23 11:25:53 -0300

    Primer ejemplo de Entity

---
+++ src/main/java/org/CCristian/hibernateapp/HibernateListar.java
@@ -0,0 +1,22 @@
+package org.CCristian.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.CCristian.hibernateapp.entity.Cliente;
+import org.CCristian.hibernateapp.util.JpaUtil;
+
+import java.util.List;
+
+public class HibernateListar {
+    public static void main(String[] args) {
+
+        /*Para poder trabajar con JPA Hibernate*/
+        EntityManager em = JpaUtil.getEntityManager();
+
+        String query = "SELECT c FROM Cliente c";
+        List<Cliente> clientes = em.createQuery(query).getResultList();
+
+        clientes.forEach(System.out::println);
+
+        em.close(); /*Cerrar conexión a la BD*/
+    }
+}
